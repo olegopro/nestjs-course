fe6a733ba76133bfda33acac9c984acefc8c5f3d
Oleg Desyatnikov <73001865+olegopro@users.noreply.github.com>
2023-10-29 13:14:38 +0300

    Добавил тип User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import { UsersService } from './users.service'
+import { CreateUserDto } from './dto/create-user.dto'
+import { User } from './entities/user.entity'
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
     @Get()
-    getUsers(): any {
+    getUsers(): User[] {
         return this.usersService.findAll()
     }
 
     @Get(':id')
-    getUserById(@Param('id') id: string): any {
+    getUserById(@Param('id') id: string): User {
         return this.usersService.findById(Number(id))
+    }
+
+    @Post()
+    createUser(@Body() body: CreateUserDto): User {
+        return this.usersService.createUser(body)
     }
 }
